8aec7cc621acfc4650e60af91ad92bf87a5fe061
Nadeem Shadan <nadeem.s@iinerds.com>
2018-12-11 18:16:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# ngEditable
+A simple directive to make HTML elements editable in angularjs
+
+NPM
+```bash
+$ npm i ngeditable
+```
+BOWER
+
+```bash
+$ bower i ngeditable
+```
+
+## How to use
+Inject in app.js 
+```js
+angular
+    .module('myApp',['ngEditable']);
+```
+add the directive to the element you want to edit
+
+```html
+<div ng-editable>
+```
+
+Help and contributions always accepted 😀
---
+++ index.js
@@ -0,0 +1,17 @@
+angular.module('ngEditable').directive('ngEditable', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, ctrl) {
+            // view -> model
+            element.bind('blur', function () {
+                scope.$apply(function () {
+                    ctrl.$setViewValue(element.html());
+                });
+            });
+            ctrl.$render = function () {
+                element.html(ctrl.$viewValue);
+            };
+            ctrl.$render();
+        }
+    };
+});
